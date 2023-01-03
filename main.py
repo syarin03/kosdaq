122e03828c22f81f481c415bde4e3eea5d467f4b
syarin03 <gami5507@gmail.com>
2023-01-03 14:04:34 +0900

    날짜 범위 지정 기능 강화 및 검색 카테고리 변경 알고리즘 진행 중

--- main.py
+++ main.py
@@ -4,30 +4,38 @@ from PyQt5.QtWidgets import *
 from PyQt5 import uic
 from PyQt5.QtCore import *
 
-# UI파일 연결
-# 단, UI파일은 Python 코드 파일과 같은 디렉토리에 위치해야한다.
 form_class = uic.loadUiType("main.ui")[0]
 
+host_str = '10.10.21.116'
+user_str = 'stock_admin'
+password_str = 'admin1234'
 
-# 화면을 띄우는데 사용되는 Class 선언
 class WindowClass(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.btn_search.clicked.connect(self.search)
+        self.date_before.dateChanged.connect(self.set_date)
+        self.date_after.dateChanged.connect(self.set_date)
+        self.search_tag.currentIndexChanged.connect(self.set_search)
+
+    def set_search(self):
+        if self.search_tag.currentText() == '날짜':
+            self.stack_search.setCurrentWidget(self.search_date)
+        if self.search_tag.currentText() == '투자자예탁금':
+            self.stack_search.setCurrentWidget(self.search_2)
+
+    def set_date(self):
+        if self.date_before.date() > self.date_after.date():
+            self.date_after.setDate(self.date_before.date())
 
     def search(self):
-        sel_combo = str(self.search_type.currentText())
-        # self.label.setText(str(self.dateEdit.date()))
-        date_str1 = self.date_before.date()
-        print(date_str1.toString('yyyy-MM-dd'))
-        date_str2 = self.date_after.date()
-        print(date_str2.toString('yyyy-MM-dd'))
-        date_str1 = date_str1.toString('yyyy-MM-dd')
-        date_str2 = date_str2.toString('yyyy-MM-dd')
+        sel_combo = str(self.search_tag.currentText())
+        date_str1 = self.date_before.date().toString('yyyy-MM-dd')
+        date_str2 = self.date_after.date().toString('yyyy-MM-dd')
+        print(date_str1, date_str2)
 
-        # search_str = str(self.label.text())
-        con = pymysql.connect(host='10.10.21.116', user='stock_admin', password='admin1234', db='stock', charset='utf8')
+        con = pymysql.connect(host=host_str, user=user_str, password=password_str, db='stock', charset='utf8')
         cur = con.cursor()
         sql = "SELECT * FROM covering WHERE " + sel_combo + " >= '" + date_str1 + "'" + " and " + sel_combo + " <= '" + date_str2 + "'"
         print(sql)
@@ -45,39 +53,13 @@ class WindowClass(QMainWindow, form_class):
             cnt1 += 1
             print()
 
-                # label_str += str(j)+', '
-        # label_str_list = label_str.split(', ')
-        # self.tableWidget.setRowCount(5)
-        # self.tableWidget.setItem(0, 0, QTableWidgetItem(label_str_list[0]))
-        # self.tableWidget.setItem(0, 1, QTableWidgetItem(label_str_list[1]))
-        # self.tableWidget.setItem(0, 2, QTableWidgetItem(label_str_list[2]))
         self.table_search.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
-        # self.label.setText(label_str)
-        # print(rows
+
         con.close()
 
 
-
 if __name__ == "__main__":
-    # QApplication : 프로그램을 실행시켜주는 클래스
     app = QApplication(sys.argv)
-
-    # WindowClass의 인스턴스 생성
     myWindow = WindowClass()
-
-    # 프로그램 화면을 보여주는 코드
     myWindow.show()
-
-    # 프로그램을 이벤트루프로 진입시키는(프로그램을 작동시키는) 코드
     app.exec_()
-
-
-# con = pymysql.connect(host='10.10.21.116', user='stock_admin', password='admin1234', db='stock', charset='utf8')
-# cur = con.cursor()
-# sql = "SELECT * FROM covering where `날짜`='2020-01-02'"
-# cur.execute(sql)
-# rows = cur.fetchall()
-# for i in rows:
-#     print(i)
-# # print(rows)
-# con.close()
