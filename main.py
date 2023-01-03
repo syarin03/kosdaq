10e6ade681e9a73622f641511ac408578f8b956d
syarin03 <gami5507@gmail.com>
2023-01-03 11:46:24 +0900

    테이블위젯에 테이블 값 출력 성공

--- main.py
+++ main.py
@@ -13,6 +13,34 @@ class WindowClass(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        con = pymysql.connect(host='10.10.21.116', user='stock_admin', password='admin1234', db='stock', charset='utf8')
+        cur = con.cursor()
+        sql = "SELECT * FROM covering"
+        cur.execute(sql)
+        rows = cur.fetchall()
+        label_str = ''
+        cnt1 = 0
+        cnt2 = 0
+        self.tableWidget.setRowCount(len(rows))
+        for i in rows:
+            for j in i:
+                print(j, end='  ')
+                self.tableWidget.setItem(cnt1, cnt2, QTableWidgetItem(str(j)))
+                cnt2 += 1
+            cnt2 = 0
+            cnt1 += 1
+            print()
+
+                # label_str += str(j)+', '
+        # label_str_list = label_str.split(', ')
+        # self.tableWidget.setRowCount(5)
+        # self.tableWidget.setItem(0, 0, QTableWidgetItem(label_str_list[0]))
+        # self.tableWidget.setItem(0, 1, QTableWidgetItem(label_str_list[1]))
+        # self.tableWidget.setItem(0, 2, QTableWidgetItem(label_str_list[2]))
+        self.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
+        self.label.setText(label_str)
+        # print(rows
+        con.close()
 
 
 if __name__ == "__main__":
@@ -29,10 +57,12 @@ if __name__ == "__main__":
     app.exec_()
 
 
-con = pymysql.connect(host='10.10.21.116', user='stock_admin', password='admin1234', db='stock', charset='utf8')
-cur = con.cursor()
-sql = "SELECT * FROM covering WHERE 날짜='2018-01-02'"
-cur.execute(sql)
-rows = cur.fetchall()
-print(rows)
-con.close()
+# con = pymysql.connect(host='10.10.21.116', user='stock_admin', password='admin1234', db='stock', charset='utf8')
+# cur = con.cursor()
+# sql = "SELECT * FROM covering where `날짜`='2020-01-02'"
+# cur.execute(sql)
+# rows = cur.fetchall()
+# for i in rows:
+#     print(i)
+# # print(rows)
+# con.close()
